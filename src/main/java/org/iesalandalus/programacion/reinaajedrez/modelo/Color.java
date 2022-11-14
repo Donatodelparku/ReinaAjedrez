69d1213959b8d37a606b8b8c16a949bc507f253b
Usuario <Usuario@PC16>
2022-11-14 12:04:42 +0100

    Hemos creado la clase posicion con los atributos fila y columna

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -2,7 +2,16 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
 
-	BLANCO,
-	NEGRO,
+	BLANCO("Blanco"),
+	NEGRO("Negro");
+
+	private String cadenaAMostrar;
 	
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+
+	public String toString() {
+		return cadenaAMostrar;
+	}
 }
