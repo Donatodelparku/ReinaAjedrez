b0774060204c009e8636fde1d5fe6b284ce66d59
Usuario <Usuario@PC16>
2022-11-14 12:15:25 +0100

    Hacemos los setters y getters de los atributos

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -2,4 +2,32 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
 
+	private int fila;
+	private char columna;
+	
+	private int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("Error, fila no válida");
+		}
+		this.fila = fila;
+	}
+	private char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("Error, columna no válida");
+		}
+		this.columna = columna;
+	}
+	
+	public static void main(String[] args) {
+		Posicion posi = new Posicion();
+		posi.setColumna('u');
+	}
+	
 }
+
