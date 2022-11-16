43bfa6a0bf1ebebcc42b9f1403b910aadad3df69
Usuario <Usuario@PC16>
2022-11-16 13:48:58 +0100

    He creado el metodo elegirColor de la clase Consola.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
 	private Consola() {
@@ -9,14 +11,28 @@ public class Consola {
 	public static void mostrarMenu() {
 		
 		System.out.println("-------- MENU TABLERO --------");
-		System.out.println("---- ELIGE UNA OPCION (1 - 4) ----");
 		System.out.println("1-> CREAR REINA POR DEFECTO");
 		System.out.println("2-> CREAR REINA ELIGIENDO COLOR");
 		System.out.println("3-> MOVER");
 		System.out.println("4-> SALIR");
 	}
 	
-	public static void main(String[] args) {
-		Consola.mostrarMenu();
+	public static void elegirOpcionMenu() {
+		
+		int opcion;
+		System.out.println("---- ELIGE UNA OPCION (1 - 4) ----");
+		do {
+			opcion = Entrada.entero();
+			if (opcion < 1 || opcion > 4) {
+				System.out.println("ERROR: Opción fuera de parámetros");
+			}
+		} while (opcion < 1 || opcion > 4);
+		
 	}
+	
+	
+	
+//	public static void main(String[] args) {
+//		Consola.mostrarMenu();
+//	}
 }
