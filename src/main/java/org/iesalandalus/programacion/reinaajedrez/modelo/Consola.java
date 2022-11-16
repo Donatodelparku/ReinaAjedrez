b9667efb12cb447985e5e0e66140d8a36d1545ff
Usuario <Usuario@PC16>
2022-11-16 14:08:17 +0100

    He creado los metodos mostrarMenuDireccion y elegirDireccion de Consola

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
@@ -5,33 +5,55 @@ import org.iesalandalus.programacion.utilidades.Entrada;
 public class Consola {
 
 	private Consola() {
-		
+
 	}
-	
+
 	public static void mostrarMenu() {
-		
 		System.out.println("-------- MENU TABLERO --------");
 		System.out.println("1-> CREAR REINA POR DEFECTO");
 		System.out.println("2-> CREAR REINA ELIGIENDO COLOR");
 		System.out.println("3-> MOVER");
 		System.out.println("4-> SALIR");
 	}
-	
-	public static void elegirOpcionMenu() {
-		
+
+	public static int elegirOpcionMenu() {
 		int opcion;
 		System.out.println("---- ELIGE UNA OPCION (1 - 4) ----");
 		do {
 			opcion = Entrada.entero();
 			if (opcion < 1 || opcion > 4) {
-				System.out.println("ERROR: Opción fuera de parámetros");
+				System.out.println("ERROR: Opción fuera de parámetros, introdúcela de nuevo: ");
 			}
 		} while (opcion < 1 || opcion > 4);
-		
+		return opcion;
+	}
+
+	public static Color elegirColor() {
+		int opcion;
+		Color color = null;
+		System.out.println("---- ELIGE UN COLOR ----");
+		System.out.println("1 <- BLANCO");
+		System.out.println("2 <- NEGRO");
+		do {
+			opcion = Entrada.entero();
+			if (opcion < 1 || opcion > 2) {
+				System.out.println("ERROR: Opción fuera de parámetros, introdúcela de nuevo");
+			}
+
+		} while (opcion < 1 || opcion > 2);
+
+		if (opcion == 1) {
+			color = Color.BLANCO;
+		} else if (opcion == 2) {
+			color = Color.NEGRO;
+		}
+		return color;
+
 	}
 	
 	
 	
+
 //	public static void main(String[] args) {
 //		Consola.mostrarMenu();
 //	}
